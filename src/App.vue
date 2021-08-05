dbcadb088516b9d4a3022349d2e40a5c9206cc36
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-05 23:22:34 +0200

    Initial setup with reset

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello from vue and vite!!!</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  text-align: center;
+}
+</style>
