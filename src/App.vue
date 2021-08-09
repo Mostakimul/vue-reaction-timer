cbce5d0af4b4fde113d1e6c22ce7db8cc0a837a8
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-09 02:54:27 +0200

    showing block and passing props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Hello from vue and vite!!!</h1>
+
+    <button @click="play" :disabled="isPlaying">Play</button>
+
+    <Block v-if="isPlaying" :delay="delay" />
   </div>
 </template>
 
 <script>
-export default {};
+import Block from './components/Block.vue';
+export default {
+  name: 'App',
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    play() {
+      this.delay = 3000 + Math.random() * 7000;
+      this.isPlaying = true;
+      // console.log(typeof this.delay);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-div {
+.container {
   text-align: center;
+}
+.container h1 {
+  color: #5a5a5a;
+  font-size: 3rem;
+  margin-top: 4rem;
+}
+.container button,
+.container button:focus {
+  outline: none;
+  border: none;
+  padding: 1rem 2.5rem;
+  margin: 1.5rem;
+  color: aliceblue;
+  background: crimson;
+  font-weight: bold;
+  font-size: 1.6rem;
+  border-radius: 0.7rem;
+  cursor: pointer;
+}
+.container button:disabled {
+  background: #dc143c5d;
+  color: #f0f8ff4d;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="box" v-if="showBlock" @click="stopTimer">
+    <h2>React Now!!!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    delay: {
+      type: Number,
+      default: 3000,
+    },
+  },
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      // console.log(this.delay);
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      // console.log(this.reactionTime);
+    },
+  },
+  // updated() {
+  //   console.log('updated block');
+  // },
+  // unmounted() {
+  //   console.log('unmounted');
+  // },
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 40rem;
+  background: crimson;
+  color: aliceblue;
+  text-align: center;
+  padding: 10rem 0;
+  margin: 4rem auto;
+  border-radius: 1.5rem;
+}
+</style>
