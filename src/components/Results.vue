441f06e0bba52767cb188e6d95c542e97ad92ac0
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-09 03:22:51 +0200

    showing rank

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>Reaction time: {{ score }}ms</p>
+    <p class="rank">{{ rank }}</p>
   </div>
 </template>
 
@@ -12,7 +13,27 @@ export default {
       default: 0,
     },
   },
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 200) {
+      this.rank = 'You are on firrreee!!!';
+    } else if (this.score < 400) {
+      this.rank = 'You are fassssst!!!';
+    } else {
+      this.rank = 'You are good!!!';
+    }
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.rank {
+  font-size: 2rem;
+  color: crimson;
+  font-weight: bold;
+}
+</style>
